6288c6b0914f72f071b9ebcc7845008676c40c7a
Ibrahim-rafiq <ibrahimnayak987@gmail.com>
2025-08-04 14:05:23 -0700

    push

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ let userNum = prompt("Guess the number that comes in the 5 times table :");
 while(userNum != gameNum){
     userNum = prompt("You entered a wrong number. Guess again:");
 };
+alert("Congratulations! You have finally won🥳🥳🥳. Please check your bank account — you must have received 10,000 dollars🤑🤑")
 
 console.log("congratulations, you entered a right 🎁🎁🎁");
